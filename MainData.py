1e6ecc8e91979002e450cb60f7eaba30f2195348
RileyAitkenhead <109069954+RileyAitkenhead@users.noreply.github.com>
2022-07-11 14:50:52 +0100

    Create MainData.py

---
+++ MainData.py
@@ -0,0 +1,18 @@
+Import Machine
+Import ScratchUi
+
+Run.Error.py
+
+If DataBase Contains ModPack.Api ==
+Set Value B == (True)
+Set Value A ==(False)
+
+If Data Is New Create New Values 
+
+      Print("NewDataCreated")
+  
+  Console Online == Relpit.py
+      If Channel 1
+    Check If Data.Stream = T
+    Else 
+    Switch Stream Until Data.Stream = F
